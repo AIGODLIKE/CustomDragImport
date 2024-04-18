20758e607404046e53fa0e7341915ee594d42fff
1029920178@qq.com <1029910278@qq.com>
2024-04-18 19:11:32 +0800

    cleanup

--- asset/scripts/post_script/importFileAsNodes.py
+++ asset/scripts/post_script/importFileAsNodes.py
@@ -7,9 +7,7 @@ event = globals().get('event')
 
 mouse_x, mouse_y = bpy.context.space_data.cursor_location
 
-tree = bpy.context.space_data.edit_tree
-if tree:
-
+if tree := bpy.context.space_data.edit_tree:
     if bpy.context.area.ui_type == 'ShaderNodeTree':
         idname = 'ShaderNodeTexImage'
     elif bpy.context.area.ui_type == 'GeometryNodeTree':
